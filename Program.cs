75f4bba3fe73ef69d7941bf9da351000a1b2b2e1
Carlos_Davila <carlos.davila@ucic.edu.mx>
2025-06-26 20:19:04 -0600

    Se completó el algoritmo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Piedra_papel_o_tijera
 Console.WriteLine("Hello, World!");
+int seleccionRobot = 0;
+Random aleatorio = new Random ();
+seleccionRobot = aleatorio.Next(1, 4); // para generar un numero del 1 al 3
+Console.WriteLine("El numero aleatorio es:"+seleccionRobot);
+Console.WriteLine("Elige 1 para piedra, 2 para papel o 3 para tijera");
+
+string texto = Console.ReadLine();
+int seleccionHumano;
+
+if (int.TryParse(texto, out seleccionHumano))
+{
+    Console.WriteLine("Número válido: " + seleccionHumano);
+}
+Console.WriteLine("El numero que escoge el humano es:"+seleccionHumano);
+
+if (seleccionHumano == seleccionRobot)
+Console.WriteLine("Empate");
+if ((seleccionHumano == 1 && seleccionRobot == 3) || (seleccionHumano == 2 && seleccionRobot == 1) || (seleccionHumano == 3 && seleccionRobot ==1 ))
+Console.WriteLine ("Gana Humano");
+else 
+Console.WriteLine ("Gana Robot");
